66a08d05012ac04aebafdf611724b13923075091
Artem <infantyev48@gmail.com>
2020-05-17 23:07:31 +0400

    Therd lesson

--- script/script.js
+++ script/script.js
@@ -8,6 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const formAnswers = document.querySelector('#formAnswers');
     const buttonNext = document.querySelector('#next');
     const buttonPrev = document.querySelector('#prev');
+    const buttonSend = document.querySelector('#send');
     
     // Массив вопросов
     const questions = [
@@ -101,6 +102,8 @@ document.addEventListener('DOMContentLoaded', () => {
     //Рендер теста
     const playTest = () => {
 
+        const finalAnswers = [];
+
         let numberQuestion = 0;
 
         //Рендер ответов
@@ -110,11 +113,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
                 //Добавление div с ответами и стилями
                 const ansverItem = document.createElement('div');
-                ansverItem.classList.add('answers-item', 'd-flex', 'flex-column');
+                ansverItem.classList.add('answers-item', 'd-flex', 'justify-conten-center');
 
                 //Генерация ответов внури div
                 ansverItem.innerHTML = `
-                <input type="${questions[index].type}" id="${answer.title}" name="answer" class="d-none">
+                <input type="${questions[index].type}" id="${answer.title}" name="answer" class="d-none" value="${answer.title}">
                 <label for="${answer.title}" class="d-flex flex-column justify-content-between">
                     <img class="answerImg" src="${answer.url}" alt="burger">
                     <span>${answer.title}</span>
@@ -127,43 +130,89 @@ document.addEventListener('DOMContentLoaded', () => {
         //Рендер вопросов
         const renderQuestions = (indexQuestion) => {
             formAnswers.innerHTML = ``;
-            questionTitle.textContent = `${questions[indexQuestion].question}`;
-            renderAnsvers(indexQuestion);
+
+            switch(true) {
+
+                case numberQuestion === 0:
+                    questionTitle.textContent = `${questions[indexQuestion].question}`
+                    renderAnsvers(indexQuestion)
+                    buttonNext.classList.remove('d-none')
+                    buttonPrev.classList.remove('d-none')
+                    buttonSend.classList.add('d-none')
+                    buttonPrev.disabled = true; 
+                break;
+
+                case numberQuestion >= 0 && numberQuestion <= questions.length-1:
+                    questionTitle.textContent = `${questions[indexQuestion].question}`
+                    renderAnsvers(indexQuestion)
+                    buttonNext.removeAttribute('disabled')
+                    buttonPrev.removeAttribute('disabled')
+                break;
+                case numberQuestion === questions.length: 
+                    buttonNext.classList.add('d-none')
+                    buttonPrev.classList.add('d-none')
+                    buttonSend.classList.remove('d-none')
+
+                    formAnswers.innerHTML = `
+                        <div class="form-group">
+                            <label for="exampleFormControlInput1">Введите номер телефона</label>
+                            <input type="email" class="form-control" id="numberPhone" placeholder="Ваш номер телефона">
+                        </div>
+                        `;
+                break;
+
+                case numberQuestion === questions.length + 1:
+                    formAnswers.textContent = ''
+                    formAnswers.textContent = 'Спасибо за заказ!'
+                    setTimeout(() => {
+                        modalBlock.classList.remove('d-block');
+                    }, 2000)
+                break;
+
+            }   
+
         }
+
         renderQuestions(numberQuestion);
 
+        const checkAnswer = () => {
+
+            const obj = {};
+
+            const inputs = [...formAnswers.elements].filter((input) => input.checked || input.id === 'numberPhone');
+            
+            inputs.forEach((input, index) =>  {
+                if (numberQuestion >= 0 && numberQuestion <= questions.length-1) {
+                    obj[`${index}_${questions[numberQuestion].question}`] = input.value;
+                }
+
+                if (numberQuestion === questions.length){
+                    obj['Номер телефона'] = input.value;
+                }
+            });
+            finalAnswers.push(obj);
+        }
 
         //Обработка клика по Next button
         buttonNext.onclick = () => {
+            checkAnswer();
             //Увеличиваем numberQuestion на 1
             numberQuestion++;
-
-            if (numberQuestion == questions.length - 1) {
-                buttonNext.disabled = true;
-            } else {
-                buttonPrev.removeAttribute('disabled');
-            };
             renderQuestions(numberQuestion);
         };
 
-        //Обработка клика по Prev button
-        //Деактивация кнопки
-        buttonPrev.disabled = true;
-
+        //Обработка клика по Prev button        
         buttonPrev.onclick = () => {
-
+            //Уменьшаем numberQuestion на -1
             numberQuestion--;
-            //Если numberQuestion = 0, то кнопка блокируется
-            if (numberQuestion == 0) {
-                buttonPrev.disabled = true;
-            } else {
-                //Кнопка Next button активна при numberQuestion != 0
-                buttonNext.removeAttribute('disabled');
-            }
             renderQuestions(numberQuestion);
         }
-
-
-
+        //Обработка клика по Send button 
+        buttonSend.onclick = () => {
+            checkAnswer();
+            numberQuestion++;
+            renderQuestions(numberQuestion);
+            console.log(finalAnswers);
+        }
     }
 });
